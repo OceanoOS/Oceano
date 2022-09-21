4f5041291845bf9365284c09dad332a799cfe721
Pto Buon <98218366+ptobuon@users.noreply.github.com>
2022-09-21 18:29:16 +0200

    Initial commit

---
+++ Boot/Kernel.cs
@@ -0,0 +1,40 @@
+﻿using Oceano.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace Oceano.Boot
+{
+    public class Kernel : Sys.Kernel
+    {
+
+        protected override void BeforeRun()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Welcome to Oceano!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+            Console.ForegroundColor= ConsoleColor.Yellow;
+            Console.WriteLine("Oceano support the filesystem. But if you are running in real hardware, it's more safe to disable it for security. Do you want to disable it? In doubt, choose no.");
+            Console.Write("Input (y/n): ");
+            var input=Console.ReadLine();
+            switch (input)
+            {
+                case "y": Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("Filesystem enabled."); Filesystem.Init(Filesystem.fs); Console.ForegroundColor = ConsoleColor.White; break;
+                case "n": Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Filesystem disabled."); Console.ForegroundColor = ConsoleColor.White; break;
+            }
+        }
+
+        protected override void Run()
+        {
+            Console.Write(">");
+            var input = Console.ReadLine();
+            switch (input)
+            {
+                case "fs info": try { Filesystem.WriteInformation(@"0:\", Filesystem.fs); } catch { Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Error: Filesystem disabled or inaccessible."); Console.ForegroundColor = ConsoleColor.White; } break;
+                
+            }
+        }
+    }
+}
---
+++ Commands/Filesystem.cs
@@ -0,0 +1,31 @@
+﻿using Cosmos.System.FileSystem;
+using Cosmos.System.FileSystem.VFS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sys = Cosmos.System;
+
+namespace Oceano.Commands
+{
+    public class Filesystem
+    {
+        public static CosmosVFS fs = new CosmosVFS();
+        public static void Init(CosmosVFS filesystem)
+        {
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(filesystem);
+        }
+        public static void WriteInformation(String drive, CosmosVFS filesystem)
+        {
+            Console.WriteLine("===== Information for " + drive + "=====");
+            Console.WriteLine();
+            var available_space = filesystem.GetAvailableFreeSpace(drive);
+            Console.WriteLine("Available Free Space: " + available_space);
+            var full_space = filesystem.GetTotalSize(drive);
+            Console.WriteLine("Total Size: " + full_space);
+            var fs_type = filesystem.GetFileSystemType(drive);
+            Console.WriteLine("File System Type: " + fs_type);
+        }
+    }
+}
