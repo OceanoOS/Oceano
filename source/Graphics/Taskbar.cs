618e9adb494cf35f58890a824f167823b171da20
Pto Buon <98218366+ptobuon@users.noreply.github.com>
2022-09-30 21:59:06 +0200

    Update

--- source/Graphics/Taskbar.cs
+++ source/Graphics/Taskbar.cs
@@ -5,14 +5,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using Mouse = Cosmos.System.MouseManager;
+using Cosmos.System;
 
 namespace Oceano.Graphics
 {
     public class Taskbar
     {
+        static bool menuopened = false;
+
         public static void DrawMenu()
         {
-            gui.canvas.DrawFilledRectangle(new(Color))
+            gui.canvas.DrawFilledRectangle(new(Color.Black), 0, 0, 800, 16);
+            gui.canvas.DrawACSIIString(new(Color.White), "File", 0, 0);
+            if (Mouse.X == 0)
+            {
+                menuopened = true;
+            }
+            if (menuopened = true)
+            {
+                
+            }
         }
     }
 }
