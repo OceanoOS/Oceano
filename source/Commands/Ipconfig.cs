31e107025205ad691b5736fa5c9cccaf18fe3d62
Pto Buon <ptobuon@outlook.it>
2022-11-14 21:42:13 +0100

    Add manual ipv4 set

--- source/Commands/Ipconfig.cs
+++ source/Commands/Ipconfig.cs
@@ -1,4 +1,6 @@
-﻿using Cosmos.System.Network.Config;
+﻿using Cosmos.HAL;
+using Cosmos.System.Network.Config;
+using Cosmos.System.Network.IPv4;
 using Cosmos.System.Network.IPv4.UDP.DHCP;
 using System;
 using System.Collections.Generic;
@@ -20,6 +22,21 @@ namespace Oceano.Commands
             {
                 switch (args[0])
                 {
+                    case "/set":
+                        try
+                        {
+                            NetworkDevice nic = NetworkDevice.GetDeviceByName("eth0");
+                            IPConfig.Enable(nic, new Address(Convert.ToByte(args[1]), Convert.ToByte(args[2]), Convert.ToByte(args[3]), Convert.ToByte(args[4])), new Address(Convert.ToByte(args[5]), Convert.ToByte(args[6]), Convert.ToByte(args[7]), Convert.ToByte(args[8])), new Address(Convert.ToByte(args[9]), Convert.ToByte(args[10]), Convert.ToByte(args[11]), Convert.ToByte(args[12]))); //enable IPv4 configuration
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            var ip = NetworkConfiguration.CurrentAddress.ToString();
+                            response = "Network configured successfully! Your local ip address: " + ip;
+                        }
+                        catch(Exception ex)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            response = ex.Message;
+                        }
+                        break;
                     case "/ask":
                         try
                         {
