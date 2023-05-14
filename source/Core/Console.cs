ef80c822a9e359ce51830adfcd57dc9d283c01e2
Pto Buon <ptobuon@outlook.it>
2023-05-14 20:09:39 +0200

    Rewrite

---
+++ source/Core/Console.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Oceano.Core
+{
+    public static class Console
+    {
+        public static void Print(string text, ConsoleColor color)
+        {
+            System.Console.ForegroundColor = color;
+            System.Console.Write(text);
+            System.Console.ResetColor();
+        }
+        public static void PrintLine(string text, ConsoleColor color)
+        {
+            System.Console.ForegroundColor = color;
+            System.Console.WriteLine(text);
+            System.Console.ResetColor();
+        }
+    }
+}
---
+++ source/Core/Program.cs
@@ -0,0 +1,17 @@
+﻿using Cosmos.System;
+using Cosmos.System.Graphics;
+
+namespace Oceano.Core
+{
+    public class Program : Kernel
+    {
+        public static Canvas canvas;
+        public static bool GraphicalMode;
+        protected override void BeforeRun()
+        {
+        }
+        protected override void Run()
+        {
+        } 
+    }
+}
