6b3c1b4b91792b58075ee9ca64cdb4b5aead2690
Pto Buon <ptobuon@outlook.it>
2023-06-29 11:51:15 +0200

    Delete docfx

--- docfx_project/api/index.md
+++
@@ -1,2 +0,0 @@
-# API documentation
-You can use this documentation to learn how is developed Oceano.
---
+++ source/Shell/Commands/General/Information.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oceano.Shell.Commands.General
+{
+    public class Information : Command
+    {
+        public Information(string name, string description) : base(name, description) { }
+        public override string Execute(string[] args)
+        {
+            return base.Execute(args);
+        }
+    }
+}
